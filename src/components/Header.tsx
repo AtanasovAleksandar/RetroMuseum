d96fbda8a1d1c4d3d54341ff0c9692385d0bce52
Aleksandar Atanasov <aleksandaratanasov43@gmail.com>
2020-08-01 23:36:08 +0200

    project setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+
+// import MenuIcon from '../assets/img/grid.svg'; 
+
+const style: any = {
+    logo: {
+        float: 'left',
+        marginLeft: '10px',
+    },
+    logoText: {
+        fontWeight: '400',
+    },
+    menu: {
+        float: 'right',
+        display: 'flex',
+        alignItems: 'center',
+        margin: '5px 20px',
+        '&:hover': {
+            background: 'red',
+        },
+    },
+    menuIcon: {
+        background: `#303030`,
+        width: '15px',
+        height: '15px',
+        color: 'gray',
+    },
+    menuText: {
+        marginLeft: '10px',
+    },
+}
+
+export const Header = () => {
+    return (
+        <div>
+            <h2 style={style.logo}>Retro<span style={style.logoText}>Museum</span></h2>
+            <div className="menu">
+                <div style={style.menuIcon}></div>
+                <h4 style={style.menuText}>MENU</h4>
+            </div>
+        </div>
+    )
+}
